2603770f92996a3820ed4e621d19e6e8d2de2375
Arina Miahkykh <79450238+Arina-M@users.noreply.github.com>
2023-05-01 10:35:34 +0300

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const EMAIL_MESSAGE_KEY = "feedback-form-state";
+
+const formEl = document.querySelector('.feedback-form');
+const { email, message } = formEl.elements;
+
+let formData = {};
+
+fillInForm()
+
+formEl.addEventListener('submit', e => {
+	e.preventDefault();
+    formData = { email: email.value, message: message.value };
+
+    console.log(formData);
+
+	localStorage.removeItem(EMAIL_MESSAGE_KEY);
+    e.currentTarget.reset();
+    formData = {};
+});
+
+formEl.addEventListener('input', throttle(() => {
+    formData = { email: email.value, message: message.value };
+    localStorage.setItem(EMAIL_MESSAGE_KEY, JSON.stringify(formData));
+}, 500))
+
+function fillInForm() {
+    if (localStorage.getItem(EMAIL_MESSAGE_KEY)) {
+        const storedData = JSON.parse(localStorage.getItem(EMAIL_MESSAGE_KEY)); 
+        email.value = storedData.email || '';
+        message.value = storedData.message || '';
+    }
+}
